393dbe1d37cdb3651b58a547c1cb3f61b51882d3
wq9578 <121522862+wq9578@users.noreply.github.com>
2024-02-26 16:12:33 +0100

    Added FlightResource

---
+++ filament-issue/app/Filament/Resources/FlightResource.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\FlightResource\Pages;
+use App\Filament\Resources\FlightResource\RelationManagers;
+use App\Models\Flight;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class FlightResource extends Resource
+{
+    protected static ?string $model = Flight::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name'),
+                Forms\Components\TextInput::make('departure'),
+                Forms\Components\TextInput::make('arrival'),
+                Forms\Components\TextInput::make('from'),
+                Forms\Components\TextInput::make('to'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name'),
+                Tables\Columns\TextColumn::make('departure'),
+                Tables\Columns\TextColumn::make('arrival'),
+                Tables\Columns\TextColumn::make('from'),
+                Tables\Columns\TextColumn::make('to'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListFlights::route('/'),
+            'create' => Pages\CreateFlight::route('/create'),
+            'edit' => Pages\EditFlight::route('/{record}/edit'),
+        ];
+    }
+}
